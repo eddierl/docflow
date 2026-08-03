ead440df73cf2373075074b2d21c2febf87c7471
Eddie Erlich <edlich6@gmail.com>
2026-08-03 22:30:21 +0800

    chore: add consolidation comment to main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,5 @@
+# SQS queues were consolidated from the removed sqs.tf file (see PR #11).
+# They are now defined inline here alongside the S3 bucket.
 resource "aws_s3_bucket" "uploads" {
   bucket = var.bucket_name
 }
